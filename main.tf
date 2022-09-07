711866df38aa940c2cc0f2e24e0e253e5a3e8f18
khasimvalishaik <33258517+khasimvalishaik@users.noreply.github.com>
2022-09-08 03:52:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@
 }
 
 provider "aws" {
-  shared_config_files      = ["~/.aws/config"]
-   shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files      = ["/home/ec2-user/.aws/config"]
+   shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
   profile = "default"
 }
 
