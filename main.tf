27b4f0ed7ff4bbb8f3fd6c9eab4f9461958541c9
khasimvalishaik <33258517+khasimvalishaik@users.noreply.github.com>
2022-09-08 03:56:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
- terraform {
- required_providers {
+terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
